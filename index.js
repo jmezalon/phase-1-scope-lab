55c3a38a76628787d948394d4c652dc2f91abb93
Jean Max Mezalon <+>
2021-10-13 23:37:00 -0400

    commit tree 3a523ca7888eec630ede13f7832c9e9566979ff9

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+    customerName = "BOB";
+    return customerName;
+}
+
+function setBestCustomer() {
+    bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob"
+}
+
+// leastFavoriteCustomer = 'joe';
+
+const leastFavoriteCustomer = 'mike';
+// function changeLeastFavoriteCustomer() {
+// }
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'andy'
+}
